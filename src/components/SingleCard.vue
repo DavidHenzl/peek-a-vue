0a748cde31359d26561e07e1143ee7e9bcbbfbb7
davidhenzl <david.henzl@gmail.com>
2022-10-07 15:57:19 +0200

    Create SingleCard component with props

---
+++ src/components/SingleCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+	props: {
+		value: {
+			type: Number,
+			required: true
+		}
+	}
+
+}
+</script>
+
+<template>
+	<div class="card">
+		{{ value }}
+	</div>
+</template>
+
+<style>
+	
+</style>
